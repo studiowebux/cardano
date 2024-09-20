7815a5002268d6dc8d83cb21e1779cea57f7866f
tommy gingras <tommy@studiowebux.com>
2024-09-19 20:48:14 -0400

    docs: improving documentation and flow for the wallet commands, working on cip-86 and indexer

---
+++ __tests__/vkey.test.ts
@@ -0,0 +1,39 @@
+import {
+  PublicKey,
+  Credential,
+  EnterpriseAddress,
+  NetworkInfo,
+} from "@emurgo/cardano-serialization-lib-nodejs";
+
+Deno.test("Testing vkey operations", () => {
+  // Key used to sign the TX
+  // From Address: addr_test1vqfe3zd3ppwpteuutqxc7g4fxk65tr5x0dv2pdkg0swvmlqpsvuxw
+  // From PKEY: ed25519_pk1majw0h3wtwf8fhyc90eh36ya357nln7w0q0lyetj2sg8pvnnqymsxa97tk
+
+  // Key stored on chain (from ogmios block in the signatories object)
+  const vkey =
+    "df64e7de2e5b9274dc982bf378e89d8d3d3fcfce781ff26572541070b2730137";
+
+  // Trying to reserve (simulating not having who signed what to get back the original address)
+  console.log(
+    "1: ",
+    PublicKey.from_bech32(
+      "ed25519_pk1majw0h3wtwf8fhyc90eh36ya357nln7w0q0lyetj2sg8pvnnqymsxa97tk",
+    ).to_hex(),
+  );
+  console.log("2: ", PublicKey.from_hex(vkey).to_bech32()); // This one returns the expected value.
+  console.log(
+    "4: ",
+    EnterpriseAddress.new(
+      NetworkInfo.testnet_preprod().network_id(),
+      Credential.from_keyhash(PublicKey.from_hex(vkey).hash()),
+    )
+      .to_address()
+      .to_bech32(),
+  ); // reverse the key stored in ogmios to get back a bech32 address
+
+  // Output
+  // 1:  df64e7de2e5b9274dc982bf378e89d8d3d3fcfce781ff26572541070b2730137
+  // 2:  ed25519_pk1majw0h3wtwf8fhyc90eh36ya357nln7w0q0lyetj2sg8pvnnqymsxa97tk
+  // 4:  addr_test1vqfe3zd3ppwpteuutqxc7g4fxk65tr5x0dv2pdkg0swvmlqpsvuxw
+});
--- src/csl/wallet.csl.ts
+++ src/csl/wallet.csl.ts
@@ -49,7 +49,7 @@ export function create_wallet(save_locally: boolean = false): {
     skey: privateKey.to_bech32(),
     skey_hex: privateKey.to_hex(),
     pkey: publicKey.to_bech32(),
-    pkey_hex: publicKey.to_hex(),
+    pkey_hex: publicKey.to_hex(), // Represent the hex that is save in ogmios transaction/signatories, useful to scan the chain searching for tx signed by this address.
     key_hash: keyHash.to_hex(),
     address_preview: enterpriseAddressPreview.to_address().to_bech32(),
     address_preprod: enterpriseAddressPreprod.to_address().to_bech32(),
@@ -71,9 +71,11 @@ export function create_wallet(save_locally: boolean = false): {
 }
 
 /**
- * Retrieves the Ed25519 key hash from a given private key.
+ * Retrieves the Ed25519 key hash instance from a given private key (ed25519_sk).
+ * When to use ?
+ * I use this with create_policy_script, to get the mint_script / NativeScript
  *
- * @param {string} private_key - The Bech32 encoded private key.
+ * @param {string} private_key - The Bech32 encoded private key. (Format: ed25519_sk...)
  * @returns {Ed25519KeyHash | undefined} The derived key hash, or undefined if invalid input.
  */
 export function get_keyhash(private_key: string): Ed25519KeyHash | undefined {
@@ -85,32 +87,39 @@ export function get_keyhash(private_key: string): Ed25519KeyHash | undefined {
 /**
  * Converts a hexadecimal address string to an {@link Address} object.
  *
- * @param {string} address - The hexadecimal encoded address.
- * @returns {Address} The parsed address object.
+ * @param {string} address - The hexadecimal encoded address, address received from a wallet in hex format.
+ * @returns {Address} The parsed address Instance (CSL).
  */
 export function get_address(address: string): Address {
   return Address.from_hex(address);
 }
 
 /**
- * Converts a Bech32 encoded private key string to a {@link PrivateKey} object.
+ * Converts a Bech32 encoded private key (ed25519_sk...) string to a {@link PrivateKey} object.
+ * When to use ?
+ * Sign your transation (witness), always use carefully !
+ * In my case I use it to mint assets
  *
- * @param {string} secret_key - The Bech32 encoded private key.
- * @returns {PrivateKey} The parsed private key object.
+ * @param {string} secret_key - The Bech32 encoded private key (Format: ed25519_sk...).
+ * @returns {PrivateKey} The parsed private key Instance.
  */
 export function get_private_key(secret_key: string): PrivateKey {
   return PrivateKey.from_bech32(secret_key);
 }
 
 /**
- * Retrieve from an address its stake key (if it has one)
+ * Retrieve from a bech32 address its stake key (if it has one)
  *
- * @param {string} address bech32 address
- * @returns {string} bech32 stake address
+ * @param {string} address bech32 address (bech32...)
+ * @param {number} network_id (0 = testnet, 1 = mainnet)
+ * @returns {string} bech32 format stake address (stake...)
  */
-export function address_to_stake(address: string): string {
+export function address_to_stake(
+  address: string,
+  network_id: number = NetworkInfo.mainnet().network_id(),
+): string {
   return RewardAddress.new(
-    NetworkInfo.mainnet().network_id(),
+    network_id,
     BaseAddress.from_address(Address.from_bech32(address))?.stake_cred()!,
   )
     .to_address()
@@ -118,11 +127,11 @@ export function address_to_stake(address: string): string {
 }
 
 /**
- * Retrieves the public key from Ed25519 key hash.
+ * Retrieves the public key from Ed25519 key hash string (Ed25519_PK).
  * Needed to get the public key from the signatories in ogmios block.
  *
  * @param {string} keyhash - The keyhash saved in a transaction (Format: Ed25519_PK...)
- * @returns {Ed25519KeyHash | undefined} The derived key hash, or undefined if invalid input.
+ * @returns {string | undefined} The derived key hash, or undefined if invalid input.
  */
 export function get_public_key_from_keyhash(
   keyhash: string,
@@ -131,8 +140,26 @@ export function get_public_key_from_keyhash(
 }
 
 /**
+ * Retrieves the public key from Ed25519 key hash.
+ * When to use ?
+ * Needed to get the key from the signatories in ogmios block.
+ * I use this with the indexer to scan transaction searching for block that have a specific signature.
+ *
+ * @param {string} public_key_bech32 - Public key (Format: Ed25519_PK...)
+ * @returns {string} Public key in hex
+ */
+export function get_public_key_hash_from_public_key(
+  public_key_bech32: string,
+): string | undefined {
+  return PublicKey.from_bech32(public_key_bech32).to_hex();
+}
+
+/**
  * From a bech32 address (with out without stake address)
  * returns the public keyhash to validate if a tx is signed by the expected address
+ *
+ * When to use ?
+ * When you need to get the public key (Ed25519_PK) from a bech32 address
  *
  * @param {string} address the bech32 address (addr...)
  * @returns {string} The Public key in bech32 format (Format: Ed25519_PK...)
